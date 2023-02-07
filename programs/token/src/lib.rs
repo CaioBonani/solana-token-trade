61a28f8acd6cd0256e20940827e20764d08f1449
Caio Bonani <caio_bonani2002@hotmail.com>
2023-02-07 17:30:59 -0300

    added smart contract and tests

--- programs/token/src/lib.rs
+++ programs/token/src/lib.rs
@@ -1,15 +1,81 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token;
+use anchor_spl::token::{Token, Transfer};
+use anchor_lang::system_program;
+use anchor_lang::error_code;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod token {
+pub mod contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn transfer_sol(ctx: Context<TransferSOL>) -> Result<()> {
+        let qtd: u64 = 1;
+        let from = ctx.accounts.from.to_account_info();
+        let to = ctx.accounts.to.to_account_info();
+
+        if **from.try_borrow_lamports()? < qtd {
+            return Err(CustomError::SaldoInfuciente.into());
+        }
+
+        // Debit from_account and credit to_account
+        **from.try_borrow_mut_lamports()? -= qtd;
+        **to.try_borrow_mut_lamports()? += qtd;
+
         Ok(())
     }
+
+    pub fn transfer_token(ctx: Context<TransferToken>) -> Result<()> {
+        let qtd: u64 = 1;
+
+        // Create the Transfer struct for our context
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.from_authority.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the Context for our Transfer request
+        let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
+
+        // Execute anchor's helper function to transfer tokens
+        anchor_spl::token::transfer(cpi_ctx, qtd)?;
+
+        Ok(())
+    }
+
+
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct TransferToken<'info> {
+    pub token_program: Program<'info, Token>,
+    /// CHECK: The associated token account that we are transferring the token from
+    #[account(mut)]
+    pub from: UncheckedAccount<'info>,
+    /// CHECK: The associated token account that we are transferring the token to
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    // the authority of the from account 
+    pub from_authority: Signer<'info>,
+}
+#[derive(Accounts)]
+pub struct TransferSOL<'info> {
+    #[account(mut)]
+    /// CHECK: This is not dangerous because its just a stackoverflow sample o.O
+    pub from: AccountInfo<'info>,
+    #[account(mut)]
+    /// CHECK: This is not dangerous because we just pay to this account
+    pub to: AccountInfo<'info>,
+    #[account()]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum CustomError {
+    #[msg("Saldo insuficiente")]
+    SaldoInfuciente,
+}
--- tests/token.ts
+++ tests/token.ts
@@ -1,16 +1,57 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { Token } from "../target/types/token";
+import { Contract } from "../target/types/contract";
+import {
+  TOKEN_PROGRAM_ID,
+  MINT_SIZE,
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+  createInitializeMintInstruction,
+} from "@solana/spl-token"; 
+import { assert } from "chai";
 
-describe("token", () => {
+describe("token-contract", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  // Retrieve the TokenContract struct from our smart contract
+  const program = anchor.workspace.TokenContract as Program<Contract>;
+  // Generate a random keypair that will represent our token
+  const mintKey: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+  // AssociatedTokenAccount for anchor's workspace wallet
+  let associatedTokenAccount = undefined;
 
-  const program = anchor.workspace.Token as Program<Token>;
+  it("Transfer token", async () => {
+    // Get anchor's wallet's public key
+    const myWallet = anchor.AnchorProvider.env().wallet.publicKey;
+    // Wallet that will receive the token 
+    const toWallet: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    // The ATA for a token on the to wallet (but might not exist yet)
+    const toATA = await getAssociatedTokenAddress(
+      mintKey.publicKey,
+      toWallet.publicKey
+    );
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    // Fires a list of instructions
+    const mint_tx = new anchor.web3.Transaction().add(
+      // Create the ATA account that is associated with our To wallet
+      createAssociatedTokenAccountInstruction(
+        myWallet, toATA, toWallet.publicKey, mintKey.publicKey
+      )
+    );
+
+    // Sends and create the transaction
+    await anchor.AnchorProvider.env().sendAndConfirm(mint_tx, []);
+
+    // Executes our transfer smart contract 
+    await program.methods.transferToken().accounts({
+      tokenProgram: TOKEN_PROGRAM_ID,
+      from: associatedTokenAccount,
+      fromAuthority: myWallet,
+      to: toATA,
+    }).rpc();
+
+    // Get minted token amount on the ATA for our anchor wallet
+    const minted = (await program.provider.connection.getParsedAccountInfo(associatedTokenAccount)).value.data.parsed.info.tokenAmount.amount;
+    assert.equal(minted, 1);
   });
 });
